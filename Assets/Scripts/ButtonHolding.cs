cd83279bc059174231dfc8b8a8f9e6ed0e0029ae
lns13301 <rkdrbgks22@ks.ac.kr>
2020-10-04 21:14:35 +0900

    feat: Add raccoon player attack animations.

---
+++ Assets/Scripts/ButtonHolding.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonHolding : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public int buttonType;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        PlayerAction.instance.SetActionType(buttonType);
+        PlayerAction.instance.animator.SetBool("isRunning", false);
+        PlayerAction.instance.isAttackButtonPressing = true;
+        // TODO 누르고 있을 때, 버튼 효과 추가
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        PlayerAction.instance.animator.SetBool("isRunning", true);
+    }
+}
--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -4,23 +4,229 @@ using UnityEngine;
 
 public class PlayerAction : MonoBehaviour
 {
-    public ActionType actionType = ActionType.ATTACK;
+    public static PlayerAction instance;
+    public Animator animator;
+    public ActionType actionType;
+    public bool isAttackButtonPressing;
+
+    public Animator mapAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
+        instance = this;
+        actionType = ActionType.RUN;
 
+        animator = transform.GetChild(0).GetComponent<Animator>();
+
+        mapAnimator = GameObject.Find("SampleGround4X").GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SetBaseState();
+        UpdateAttackState();
+        SetAnimationState(actionType);
     }
 
     private void OnPlayerWalk(Animator animator, string animationName)
     {
         animator.SetBool(animationName, true);
+    }
+
+    public void SetActionType(int value)
+    {
+        if (!IsCurrentAnimation(animator, "PlayerRun"))
+        {
+            return;
+        }
+
+        switch (value)
+        {
+            case 0:
+                actionType = ActionType.NONE;
+                break;
+            case 1:
+                actionType = ActionType.IDLE;
+                break;
+            case 2:
+                actionType = ActionType.WALK;
+                break;
+            case 3:
+                actionType = ActionType.RUN;
+                break;
+            case 4:
+                actionType = ActionType.JUMP;
+                break;
+            case 5:
+                actionType = ActionType.DOUBLEJUMP;
+                break;
+            case 6:
+                actionType = ActionType.TRIPLEJUMP;
+                break;
+            case 7:
+                actionType = ActionType.UNDERJUMP;
+                break;
+            case 8:
+                actionType = ActionType.ATTACK;
+                break;
+            case 9:
+                actionType = ActionType.ATTACK1;
+                break;
+            case 10:
+                actionType = ActionType.ATTACK2;
+                break;
+            case 11:
+                actionType = ActionType.ATTACK3;
+                break;
+            case 12:
+                actionType = ActionType.ATTACK4;
+                break;
+            case 13:
+                actionType = ActionType.ATTACK5;
+                break;
+            case 14:
+                actionType = ActionType.SKILL;
+                break;
+            case 15:
+                actionType = ActionType.SKILL1;
+                break;
+            case 16:
+                actionType = ActionType.SKILL2;
+                break;
+            case 17:
+                actionType = ActionType.ULTIMATE;
+                break;
+        }
+    }
+
+    public void SetBaseState()
+    {
+        if (actionType == ActionType.NONE)
+        {
+            actionType = ActionType.RUN;
+            ReSetTriggers();
+        }
+    }
+
+    public void SetAnimationState(ActionType actionType)
+    {
+        switch (actionType)
+        {
+            case ActionType.RUN:
+                animator.SetBool("isRunning", true);
+                ReSetTriggers();
+                mapAnimator.SetFloat("GroundSpeed", 2.0f);
+                break;
+            case ActionType.ATTACK:
+                animator.SetTrigger("doAttack");
+                mapAnimator.SetFloat("GroundSpeed", 0.5f);
+                break;
+            case ActionType.ATTACK1:
+                animator.SetTrigger("doAttack1");
+                break;
+            case ActionType.ATTACK2:
+                animator.SetTrigger("doAttack2");
+                break;
+            case ActionType.ATTACK3:
+                animator.SetTrigger("doAttack3");
+                break;
+            case ActionType.ATTACK4:
+                animator.SetTrigger("doAttack4");
+                break;
+        }
+    }
+
+    public void ReSetTriggers()
+    {
+        animator.ResetTrigger("doAttack");
+        animator.ResetTrigger("doAttack1");
+        animator.ResetTrigger("doAttack2");
+        animator.ResetTrigger("doAttack3");
+        animator.ResetTrigger("doAttack4");
+    }
+
+    public void UpdateAttackState()
+    {
+        float value = GetAnimationNormalizedTime(animator);
+
+        switch (actionType)
+        {
+            case ActionType.ATTACK:
+                if (IsCurrentAnimation(animator, "Attack"))
+                {
+                    if (isAttackButtonPressing && value >= 0.35f && value <= 0.7f)
+                    {
+                        actionType = ActionType.ATTACK1;
+                    }
+                    else if (value > 0.75f)
+                    {
+                        actionType = ActionType.RUN;
+                    }
+                }
+                break;
+            case ActionType.ATTACK1:
+                if (IsCurrentAnimation(animator, "Attack 1"))
+                {
+                    if (isAttackButtonPressing && value >= 0.35f && value <= 0.7f)
+                    {
+                        actionType = ActionType.ATTACK2;
+                    }
+                    else if (value > 0.75f)
+                    {
+                        actionType = ActionType.RUN;
+                    }
+                }
+                break;
+            case ActionType.ATTACK2:
+                if (IsCurrentAnimation(animator, "Attack 2"))
+                {
+                    if (isAttackButtonPressing && value >= 0.35f && value <= 0.7f)
+                    {
+                        actionType = ActionType.ATTACK3;
+                    }
+                    else if (value > 0.75f)
+                    {
+                        actionType = ActionType.RUN;
+                    }
+                }
+                break;
+            case ActionType.ATTACK3:
+                if (IsCurrentAnimation(animator, "Attack 3"))
+                {
+                    if (isAttackButtonPressing && value >= 0.6f && value <= 0.9f)
+                    {
+                        actionType = ActionType.ATTACK4;
+                    }
+                    else if (value > 0.9f)
+                    {
+                        actionType = ActionType.RUN;
+                    }
+                }
+                break;
+            case ActionType.ATTACK4:
+                if (IsCurrentAnimation(animator, "Attack 4"))
+                {
+                    if (value > 0.75f)
+                    {
+                        actionType = ActionType.RUN;
+                    }
+                }
+                break;
+        }
+
+        isAttackButtonPressing = false;
+    }
+
+    public bool IsCurrentAnimation(Animator animator, string animationName)
+    {
+        return animator.GetCurrentAnimatorStateInfo(0).IsName(animationName);
+    }
+
+    public float GetAnimationNormalizedTime(Animator animator)
+    {
+        return animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
     }
 }
 
@@ -32,5 +238,16 @@ public enum ActionType
     RUN,
     JUMP,
     DOUBLEJUMP,
-    ATTACK
+    TRIPLEJUMP,
+    UNDERJUMP,
+    ATTACK,
+    ATTACK1,
+    ATTACK2,
+    ATTACK3,
+    ATTACK4,
+    ATTACK5,
+    SKILL,
+    SKILL1,
+    SKILL2,
+    ULTIMATE
 }
