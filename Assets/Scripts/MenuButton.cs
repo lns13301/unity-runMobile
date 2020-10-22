bc3d786b3536fb56880a585c1a88faa9f2f5a696
lns13301 <rkdrbgks22@ks.ac.kr>
2020-10-22 22:42:39 +0900

    feat: Add menu button, Hero select UI.

---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+    private Animator animator;
+    private Animator heroUIAnimator;
+    public bool isUIOn;
+    private int menuButtonChildCount;
+
+    public GameObject heroUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = transform.parent.GetComponent<Animator>();
+        heroUIAnimator = heroUI.GetComponent<Animator>();
+        isUIOn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ButtonMenu()
+    {
+        if (isUIOn)
+        {
+            animator.SetBool("isMenuOn", false);
+            isUIOn = false;
+        }
+        else
+        {
+            animator.SetBool("isMenuOn", true);
+            isUIOn = true;
+        }
+    }
+
+    public void ButtonHeroUIOnOff()
+    {
+        if (heroUI.activeSelf)
+        {
+            heroUI.SetActive(false);
+        }
+        else
+        {
+            heroUI.SetActive(true);
+            heroUIAnimator.SetTrigger("doUIOn");
+            Invoke("OnCharacterIllust", 0.5f);
+        }
+    }
+
+    public void OnCharacterIllust()
+    {
+        heroUIAnimator.SetTrigger("doCharacterIllustOn");
+        CancelInvoke("OnCharacterIllust");
+    }
+}
